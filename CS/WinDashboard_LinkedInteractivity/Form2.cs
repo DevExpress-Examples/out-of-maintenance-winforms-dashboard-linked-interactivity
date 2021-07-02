d938a57cce6b347bb48f8d86ccc51c2e29eae65a
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2021-07-02 17:40:06 +0300

    Update Form2.cs

--- CS/WinDashboard_LinkedInteractivity/Form2.cs
+++ CS/WinDashboard_LinkedInteractivity/Form2.cs
@@ -1,18 +1,14 @@
-﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardCommon;
 using DevExpress.DashboardCommon.ViewerData;
 using DevExpress.DashboardWin;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 
-namespace WinDashboard_LinkedInteractivity
-{
-    public partial class Form2 : DevExpress.XtraEditors.XtraForm
-    {
-        public new Form1 ParentForm { get; set; }
-        
-        public Form2(Form1 parentForm)
-        {
+namespace WinDashboard_LinkedInteractivity {
+    public partial class Form2 : DevExpress.XtraEditors.XtraForm {
+        public new Form1 ParentForm { get; set; }        
+        public Form2(Form1 parentForm) {
             InitializeComponent();
             this.dashboardViewer1.LoadDashboard("Data\\DashboardChild.xml");
             this.ParentForm = parentForm;
@@ -23,16 +19,13 @@ namespace WinDashboard_LinkedInteractivity
             dParentControl.DrillUpPerformed += DControl_DrillUpPerformed;
         }
 
-        bool HasDashboardItem(string itemName)
-        {
+        bool HasDashboardItem(string itemName) {
             return this.dashboardViewer1.Dashboard.Items.
                 Select(i => i.ComponentName).Contains(itemName);
         }
 
-        private void DControl_MasterFilterSet(object sender, MasterFilterSetEventArgs e)
-        {
-            if (HasDashboardItem(e.DashboardItemName))
-            {
+        private void DControl_MasterFilterSet(object sender, MasterFilterSetEventArgs e) {
+            if (HasDashboardItem(e.DashboardItemName)) {
                 string itemName = e.DashboardItemName;
                 if (e.SelectedValues != null)
                     this.dashboardViewer1.SetMasterFilter(itemName, e.SelectedValues);
@@ -40,44 +33,35 @@ namespace WinDashboard_LinkedInteractivity
                     this.dashboardViewer1.SetRange(itemName, e.SelectedRange);
             }
         }
-        private void DControl_MasterFilterCleared(object sender, MasterFilterClearedEventArgs e)
-        {
-            if (HasDashboardItem(e.DashboardItemName))
-            {
+        private void DControl_MasterFilterCleared(object sender, MasterFilterClearedEventArgs e) {
+            if (HasDashboardItem(e.DashboardItemName)) {
                 string itemName = e.DashboardItemName;
                 if (this.dashboardViewer1.CanClearMasterFilter(itemName))
                     this.dashboardViewer1.ClearMasterFilter(itemName);
             }
         }
-        private void DControl_DrillDownPerformed(object sender, DrillActionEventArgs e)
-        {
-            if (HasDashboardItem(e.DashboardItemName))
-            {
+        private void DControl_DrillDownPerformed(object sender, DrillActionEventArgs e) {
+            if (HasDashboardItem(e.DashboardItemName)) {
                 string itemName = e.DashboardItemName;
                 DashboardDataRow row = e.Values[0];
                 object value = row[row.Length - 1];
 
                 IList<AxisPointTuple> tuple = this.dashboardViewer1.GetAvailableDrillDownValues(itemName);
                 IEnumerable<object> availableValues = tuple.Select(t => t.GetAxisPoint().UniqueValue);
-                if (availableValues.Contains(value))
-                {
+                if (availableValues.Contains(value)) {
                     this.dashboardViewer1.PerformDrillDown(e.DashboardItemName, value);
                 }
             }
         }
-        private void DControl_DrillUpPerformed(object sender, DrillActionEventArgs e)
-        {
-            if (HasDashboardItem(e.DashboardItemName))
-            {
+        private void DControl_DrillUpPerformed(object sender, DrillActionEventArgs e) {
+            if (HasDashboardItem(e.DashboardItemName)) {
                 string itemName = e.DashboardItemName;
                 int level = e.DrillDownLevel;
                 AxisPointTuple tuple = this.dashboardViewer1.GetCurrentDrillDownValues(itemName);
-                if (tuple != null)
-                {
+                if (tuple != null) {
                     AxisPoint point = this.dashboardViewer1.GetCurrentDrillDownValues(itemName).GetAxisPoint();
                     int l = 0;
-                    while (point.Parent != null)
-                    {
+                    while (point.Parent != null) {
                         l++;
                         point = point.Parent;
                     }
